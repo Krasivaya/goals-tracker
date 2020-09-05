1de78dc346a128ec386c1a017ed9b20e6f5349e5
Krasivaya <semwagacarine@gmail.com>
2020-09-05 21:47:00 +0200

    Add an event for adding a goal

--- js/index.js
+++ js/index.js
@@ -11,4 +11,14 @@ function addItem(e) {
 
     // Get input value
     const newItem = document.querySelector('#item').value;
+
+    // Create new li element
+    const li = document.createElement('li');
+    // Add a className
+    li.className = 'list-group-item custom-checkbox';
+    // Add text Node
+    li.innerHTML = `
+    <input type="checkbox" name="goal" class="goalCheck" id="${newItem}">
+    <label class="yoo pl-2" for="${newItem}">${newItem}</label>
+    `;
 }
